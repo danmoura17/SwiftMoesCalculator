3ea40d68ac64117499398eb13069c2c5fb6936d2
Daniel Moura <danmoura17@gmail.com>
2019-11-02 15:03:00 -0300

    Implementing models

---
+++ MoesCalculator/Models/BarItem.swift
@@ -0,0 +1,19 @@
+//
+//  BarItem.swift
+//  MoesCalculator
+//
+//  Created by Bruno Rodrigues on 02/11/19.
+//  Copyright © 2019 Daniel Moura. All rights reserved.
+//
+
+import Foundation
+
+class BarItem{
+    
+    var title: String
+    
+    init(title :String){
+        self.title = title
+    }
+    
+}
---
+++ MoesCalculator/Models/OrderList.swift
@@ -0,0 +1,20 @@
+//
+//  OrderList.swift
+//  MoesCalculator
+//
+//  Created by Bruno Rodrigues on 02/11/19.
+//  Copyright © 2019 Daniel Moura. All rights reserved.
+//
+
+import Foundation
+
+class OrderList{
+    
+    var title :String!
+    var barItems :[BarItem] = [BarItem]()
+    
+    init(title: String){
+        self.title = title
+    }
+    
+}
